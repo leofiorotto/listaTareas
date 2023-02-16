c708838968afb77c727f4c8ec2c62059c3fb3e5c
Leandro <leo.fiorotto@hotmail.com>
2023-02-16 15:53:57 -0300

    lista de tareas 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 import ListaDeTareas from './componentes/ListaDeTareas';
 
 function App() {
   return (
     <div className='aplicacion-tareas'>
       <div className='freecodecamp-logo-contenedor'>
-        <img 
-          src={freeCodeCampLogo} 
-          className='freecodecamp-logo' />
+        
       </div>
       <div className='tareas-lista-principal'>
         <h1>Mis Tareas</h1>
